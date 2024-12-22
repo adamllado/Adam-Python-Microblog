f9cea998a9fae8fc978fde6a8646096cec964e17
adamllado <adamllado@outlook.com>
2024-12-22 00:09:54 -0500

    Changes: Added Templates Folder with html files - Added base.html which includes redundant html code for the homepage of the microblog. - Added an index.html which extends base and includes block content that adds information about posts made by users and added to the base.html file - Added business logic to routes.py which includes some mock users to display their names and what they posted

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Adam'}
+    posts = [
+        {
+            'author': {'username': 'Monica'},
+            'body': "My bf tells me how much my ass turns him on! Should I show him a pic tehe??"
+            
+        },
+        {
+            'author': {'username': 'Michelle'},
+            'body': "Monica, you know flat asses don't turn anyone on gurl. stop lying to yourself Mhmm."
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
