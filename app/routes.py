43ec31c1b4d054badc5a5e2c22eb890b167014e4
adamllado <adamllado@outlook.com>
2024-12-27 17:00:58 -0500

    Changes: Added a Sign Page - Created a forms.py which is a Class that defines the variables of the Class LoginForm. Validators are used for the username and password - Added a routes path to the Sign In page using /login as the browser path and defining how the login function will work and validate the login credentials. Uses methods GET and POST to send data to the server using POST and the data from server using GET. - Added a config.py file to define a secret key to protect the website from CSRF attacks - Added that key to the init.py file - Created a login html file that allows users to give login credentials and use a remember me button. validators check if login credentials are input and will return an error if not. - replacesd direct path names in base.html to use Flask method url_for() which returns the correct mapping making it easier to manage in the long run if I decide to change website layout. - updated posts on the homepage featuring a funny exhcange between George Foreman and Muhammed Ali.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,14 +8,22 @@ def index():
     user = {'username': 'Adam'}
     posts = [
         {
-            'author': {'username': 'Monica'},
-            'body': "My bf tells me how much my ass turns him on! Should I show him a pic tehe??"
+            'author': {'username': 'George Foreman'},
+            'body': "If only I had my Foreman grill back in the day I could've slapped Ali silly with the greatest burgers he had."
             
         },
         {
-            'author': {'username': 'Michelle'},
-            'body': "Monica, you know flat asses don't turn anyone on gurl. stop lying to yourself Mhmm."
+            'author': {'username': 'Muhammed Ali from the Grave'},
+            'body': "What was that George? You're cooking me dinner? I knew you were a girl all along!"
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+    
